becc418c92d4f125486eae623d89614e08e46ad5
Jameson Blake Collins <jblakecollins84@gmail.com>
2024-06-12 19:32:28 -0500

    Setup user model with validators

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+# email:string
+# password_digest:string
+#
+# password:string_virtual
+# password_confirmation:string_virtual
+
+class User < ApplicationRecord
+    has_secure_password # runs through bcrypt gem
+    has_many :posts
+    has_many :comments
+    has_many :likes
+    has_many :liked_posts, through: :likes, source: :post
+    has_many :liked_comments, through: :likes, source: :comment
+    validates :email, format: { with: /^[\w\-\.]+@([\w\-]+\.)+[\w\-]{2,4}$/, message: "Must be a valid email address" }, presence: true, uniqueness: true # must have unique email
+    validates :username, presence: true, uniqueness: true # must have unique username
+    validates :password, length: { minimum: 6 }, allow_nil: true
+    before_save :downcase_email
+    before_save :downcase_username
+
+    def downcase_email
+        self.email = email.downcase
+    end
+
+    def downcase_username
+        self.username = username.downcase
+    end
+end
